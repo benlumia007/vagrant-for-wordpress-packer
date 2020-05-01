59230ab689e801ead81109d30ffd94d715ec9784
Benjamin Lu <benlumia007@users.noreply.github.com>
2020-04-30 22:30:42 -0700

    update composer install

--- scripts/composer.sh
+++ scripts/composer.sh
@@ -5,7 +5,8 @@ noroot() {
 }
 
 noroot php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-noroot php -r "if (hash_file('sha384', 'composer-setup.php') === '48e3236262b34d30969dca3c37281b3b4bbe3221bda826ac6a9a62d6444cdb0dcd0615698a5cbe587c3f0fe57a54d8f5') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+noroot php -r "if (hash_file('sha384', 'composer-setup.php') === 'e0012edf3e80b6978849f5eff0d4b4e4c79ff1609dd1e613307e16318854d24ae64f26d17af3ef0bf7cfb710ca74755a') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
 noroot php composer-setup.php
 noroot php -r "unlink('composer-setup.php');"
+noroot chmod +x composer.phar
 sudo mv composer.phar /usr/local/bin/composer
