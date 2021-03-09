6693236309c3362db5cbe532f57772d5a08887a1
benlumia007 <benlumia007@users.noreply.github.com>
2021-03-09 09:17:04 -0800

    Update php-custom.sh

--- scripts/php-custom.sh
+++ scripts/php-custom.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -eux
 
 cd /etc/php/7.2/mods-available
-sudo wget https://benjlu.com/downloads/php-custom.ini
+sudo wget https://gist.githubusercontent.com/benlumia007/11eb65c4241b9efb2af337ca69c7f037/raw/bfeadd6335e85b5ba5ab05985623cd13ce45e033/php-custom.ini
 sudo phpenmod php-custom
 sudo systemctl restart nginx
