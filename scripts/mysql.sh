16467805815694c9a66cac989fbf9334d3614189
benlumia007 <benlumia007@users.noreply.github.com>
2021-03-09 09:14:49 -0800

    Update mysql.sh

--- scripts/mysql.sh
+++ scripts/mysql.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -eux
 
-sudo wget https://benjlu.com/downloads/.my.cnf;
+sudo wget https://gist.githubusercontent.com/benlumia007/48fc2bdfa87d66af961649fb68577ea9/raw/76ba9fbb5ad88bbde039ee4c324fcc6a3d6df098/.my.cnf;
 sudo chmod 0775 .my.cnf;
 
 sudo mysql -uroot -proot -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH 'mysql_native_password' BY 'root';";
@@ -8,7 +8,7 @@ sudo mysql -uroot -proot -e "FLUSH PRIVILEGES;";
 
 cd /etc/mysql;
 sudo rm mysql.cnf;
-sudo wget https://benjlu.com/downloads/mysql.cnf;
+sudo wget https://gist.githubusercontent.com/benlumia007/48fc2bdfa87d66af961649fb68577ea9/raw/76ba9fbb5ad88bbde039ee4c324fcc6a3d6df098/mysql.cnf;
 sudo systemctl restart mysql;
 
 cd /var/log/mysql;
