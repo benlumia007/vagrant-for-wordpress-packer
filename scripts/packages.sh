a6ca843f262b7ecce8e6faaff4b7c2c1d43e3f0c
benlumia007 <benlumia007@users.noreply.github.com>
2021-03-10 07:28:18 -0800

    Update packages.sh
    php7.2 to php7.4

--- scripts/packages.sh
+++ scripts/packages.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -eux
 
 sudo apt-get install php-imagick php-memcache php-memcached php-pear php-ssh2 php-xdebug php-yaml -y;
-sudo apt-get install php7.2-bcmath php7.2-cli php7.2-common php7.2-curl php7.2-dev php7.2-fpm php7.2-gd php7.2-imap php7.2-json php7.2-mbstring php7.2-mysql php7.2-soap php7.2-sqlite3 php7.2-xml php7.2-zip -y;
+sudo apt-get install php7.4-bcmath php7.4-cli php7.4-common php7.4-curl php7.4-dev php7.4-fpm php7.4-gd php7.4-imap php7.4-json php7.4-mbstring php7.4-mysql php7.4-soap php7.4-sqlite3 php7.4-xml php7.4-zip -y;
 sudo apt-get install nginx mysql-server -y;
 sudo apt-get install curl gettext g++ git git-lfs git-svn graphviz imagemagick make memcached ngrep nodejs ntp python-pip subversion unzip zip -y;
 sudo apt-get install npm -y;
