3af301683410be5a3fe9b15d5eb4502e8dbbb039
benlumia007 <benlumia007@users.noreply.github.com>
2021-03-10 08:12:34 -0800

    Update mailhog.sh

--- scripts/mailhog.sh
+++ scripts/mailhog.sh
@@ -10,7 +10,7 @@ sudo wget https://gist.githubusercontent.com/benlumia007/4648124b5ccc8b7b5b64c0a
 sudo systemctl enable mailhog
 sudo systemctl start mailhog
 
-cd /etc/php/7.2/mods-available
+cd /etc/php/7.4/mods-available
 sudo wget https://gist.githubusercontent.com/benlumia007/5d13c52bd300a9234077978c075c4ff3/raw/c0d46366d62d6894f876993c21e2f416b252e596/mailhog.ini
 sudo phpenmod mailhog
 sudo systemctl restart nginx
