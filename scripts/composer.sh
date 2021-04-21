454ab59edf1cc3e19ccf5debd3b293d1b7f3de27
Benjamin Lu <benlumia007@users.noreply.github.com>
2021-04-21 16:13:59 -0700

    add back and set 2.0.4 as default

--- scripts/composer.sh
+++ scripts/composer.sh
@@ -4,9 +4,6 @@ noroot() {
     sudo -EH -u "vagrant" "$@";
 }
 
-noroot php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-noroot php -r "if (hash_file('sha384', 'composer-setup.php') === 'e0012edf3e80b6978849f5eff0d4b4e4c79ff1609dd1e613307e16318854d24ae64f26d17af3ef0bf7cfb710ca74755a') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
-noroot php composer-setup.php
-noroot php -r "unlink('composer-setup.php');"
+noroot wget https://getcomposer.org/download/2.0.4/composer.phar
 noroot chmod +x composer.phar
 sudo mv composer.phar /usr/local/bin/composer
