07b9b0edcf0353ed5b47e151b014833b72a2c49d
Benjamin Lu <benlumia007@users.noreply.github.com>
2021-04-21 10:21:27 -0700

    removed php-xdebug

--- scripts/packages.sh
+++ scripts/packages.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -eux
 
-sudo apt-get install php-imagick php-memcache php-memcached php-pear php-ssh2 php-xdebug php-yaml -y;
+sudo apt-get install php-imagick php-memcache php-memcached php-pear php-ssh2 php-yaml -y;
 sudo apt-get install php7.4-bcmath php7.4-cli php7.4-common php7.4-curl php7.4-dev php7.4-fpm php7.4-gd php7.4-imap php7.4-json php7.4-mbstring php7.4-mysql php7.4-soap php7.4-sqlite3 php7.4-xml php7.4-zip -y;
 sudo apt-get install nginx mysql-server -y;
 sudo apt-get install curl gettext g++ git git-lfs git-svn graphviz imagemagick make memcached ngrep nodejs ntp python-pip subversion unzip zip -y;
